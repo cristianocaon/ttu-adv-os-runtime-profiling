cf4778e50dfc89330cf208dcdf7577b6abb86fb4
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-03-06 16:11:20 -0600

    remove menu

--- util.py
+++ util.py
@@ -2,20 +2,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def menu():
-    print(
-        "\nPlease, run the program with one of the options below as argument: \n(E.g. >>> python3 main.py 1)")
-    print('''
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    | Select option:              |
-    |  1- Run only QuickSort      |
-    |  2- Run only MergeSort      |
-    |  3- Run both concurrently   |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    ''')
-    print("User and system level info will be displayed.\n")
-
-
 def print_array(arr, sorting=None):
     intro_msg = f"\nSorted array with {sorting}:" if sorting else "\nUnsorted array:"
     print(intro_msg)
